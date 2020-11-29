677fd806cb9edc115620464449bba879c21f1627
Louis Liu <louis_liu2@apple.com>
2020-11-28 16:38:12 -0800

    UnionFind skeleton

---
+++ Disjoint Sets/src/DisjointSets.java
@@ -0,0 +1,6 @@
+public interface DisjointSets {
+    /* Connect two items P and Q */
+    void connect(int p, int q) ;
+    /* Checks to see if two items are connected */
+    boolean isConnected(int p, int q);
+}
